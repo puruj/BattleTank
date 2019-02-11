e1eca7f13fc49a25bdda193a075e6d26f934aa12
Puru Jetly <puru.jetly@rubiconpublishing.com>
2019-02-11 16:52:16 -0500

    can find pixel location of crosshar

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -16,9 +16,34 @@ void  ATankPlayerController::BeginPlay() {
 	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
 }
 
+void ATankPlayerController::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+	AimTowardsCrosshair();
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+void  ATankPlayerController::AimTowardsCrosshair() {
+	if (!GetControlledTank()) {return;}
+	FVector OutHitLocation; //Out Parameter
+	if (GetSightRayHitLocation(OutHitLocation)) {
+		//UE_LOG(LogTemp, Warning, TEXT("LookDirection: %s"), *OutHitLocation.ToString());
+			//Tell tank aim at that point
+	}
+}
+//Get world location of linetrace through crosshair, true if hits landscape
+bool  ATankPlayerController::GetSightRayHitLocation(FVector& OutHitLocation) const {
+	//find crosshair pos
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	auto ScreenLocation = FVector2D(ViewportSizeX * CrossHairXLocation, ViewportSizeY * CrossHairYLocation);
+	//Set screen position of crosshair to world direction
+	//Line trace aloong that direction, and check what is hit
+
+	return true;
 }
 
 
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -15,8 +15,21 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
-public:
+private:
 	ATank* GetControlledTank() const;
 
 	virtual void BeginPlay() override;
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	//move turret so it hit aims towards crosshair
+	void AimTowardsCrosshair();
+	//Return OUT parameter, true if landscape hit
+	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
+	UPROPERTY(EditAnywhere)
+	float CrossHairXLocation = 0.5;
+	UPROPERTY(EditAnywhere)
+	float CrossHairYLocation = 0.33333;
+	
+
 };
